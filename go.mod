c5f159ba4b869f3ff2f747b561c51ac4b995976d
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2023-11-28 21:56:06 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jdlms/go-api
+
+go 1.21.4
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/gorilla/mux v1.8.1
+)
---
+++ models.go
@@ -0,0 +1,46 @@
+package main
+
+type Book struct {
+	ID               string `json:"id"`
+	Title            string `json:"title"`
+	Author           string `json:"author"`
+	PublishedDate    string `json:"published_date"`
+	OriginalLanguage string `json:"original_language"`
+}
+
+var books = []*Book{
+	{
+		ID:               "1",
+		Title:            "7 habits of Highly Effective People",
+		Author:           "Stephen Covey",
+		PublishedDate:    "15/08/1989",
+		OriginalLanguage: "English",
+	},
+}
+
+func listBooks() []*Book {
+	return books
+}
+
+func GetBook(id string) *Book {
+	for _, book := range books {
+		if book.ID == id {
+			return book
+		}
+	}
+	return nil
+}
+
+func StoreBook(book Book) {
+	books = append(books, &book)
+}
+
+func DeleteBook(id string) string {
+	for i, book := range books {
+		if book.ID == id {
+			books = append(books[:i], (books)[i+1:]...)
+			return id
+		}
+	}
+	return ""
+}
